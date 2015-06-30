d2d59ea3ca9b7b7d085105d353b28981c338ed12
Move On <moveon@Moves-MacBook-Pro.local>
2015-06-30 13:09:16 -0700

    Initial Commit

---
+++ Celebrity Crush/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Celebrity Crush
+//
+//  Created by Move On on 6/30/15.
+//  Copyright (c) 2015 Charm Lopez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Celebrity Crush/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Celebrity Crush
+//
+//  Created by Move On on 6/30/15.
+//  Copyright (c) 2015 Charm Lopez. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
